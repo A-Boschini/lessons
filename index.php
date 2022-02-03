9c79d5ebf8adf534b13439f69018d54e26f049e2
Alberto <albertoboschinicr@gmail.com>
2022-02-03 15:25:32 -0600

    INSERT-1

--- index.php
+++ index.php
@@ -12,16 +12,10 @@
     <body>
 
 <?php
-    $sql = "SELECT * FROM users;";
-    $result = mysqli_query($conn, $sql);
-    $resultCheck = mysqli_num_rows($result);
-
-    if ($resultCheck > 0){
-        while ($row = mysqli_fetch_assoc($result)) {
-            echo $row['USER_uid'] . "<br>";
-        }
-    }
- ?>
+    $sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd)
+    VALUES ('Gino', 'Chaves', "lineaycolor77@yahoo.com', 'Arte', 'Chirris123');";
+    mysqli_query($conn, $sql);
+?>
 
     </body>
 </html>
