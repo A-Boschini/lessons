2e913d5fb2adda5baf3202587fc1700bb899ec1a
Alberto <albertoboschinicr@gmail.com>
2022-02-04 14:47:23 -0600

    jueves 4 de febrero 2022

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
-<?php
-    include_once 'includes/dbh.inc.php';
- ?>
-
-
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,11 +5,19 @@
     </head>
     <body>
 
-<?php
-    $sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd)
-    VALUES ('Gino', 'Chaves', "lineaycolor77@yahoo.com', 'Arte', 'Chirris123');";
-    mysqli_query($conn, $sql);
-?>
+<form action="includes/signup.inc.php" method="POST">
+    <input type="text" name="first" placeholder="Firstname">
+    <br>
+    <input type="text" name="last" placeholder="Lastname">
+    <br>
+    <input type="text" name="email" placeholder="E-mail">
+    <br>
+    <input type="text" name="uid" placeholder="Username">
+    <br>
+    <input type="password" name="pwd" placeholder="Password">
+    <br>
+    <button type="submit" name="submit">Sign Up</button>
+</form>
 
-    </body>
+</body>
 </html>
