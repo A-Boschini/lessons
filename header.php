402342e60ad8cc68c3e73557db0eb0f3391209b6
Alberto Boschini <albertoboschinicr@gmail.com>
2022-02-11 08:50:31 -0600

    Login System 01-2

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>PHP project 01</title>
+        <link rel="stylesheet" href="/css/reset.css">
+        <link rel="stylesheet" href="/css/style.css">
+    </head>
+    <body>
+
+        <nav>
+            <div class="wrapper">
--- index.php
+++ index.php
@@ -1,15 +1,7 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <title>PHP project 01</title>
-        <link rel="stylesheet" href="/css/reset.css">
-        <link rel="stylesheet" href="/css/style.css">
-    </head>
-    <body>
+<?php
+    include_once 'header.php';
 
-        <nav>
-            <div class="wrapper">
+ ?>
                 <ul>
                     <li><a href="index.php">Home</a></li>
                     <li><a href="discover.php">About Us</a></li>
@@ -17,10 +9,7 @@
                     <li><a href="signup.php">Sign Up</a></li>
                     <li><a href="login.php">LOG IN</a></li>
                 </ul>
-
-            </div>
-        </nav>
-
-    </body>
-</html>
-<script src="js/script.js"></script>
+            </nav>
+        <?php
+            include_once 'footer.php';
+         ?>
